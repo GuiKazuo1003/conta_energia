4ed6f6f5627e0f96b35f1fcfa47c52d6b9219a08
GuiKazuo1003 <guilhermeiama@gmail.com>
2023-04-18 23:45:30 -0300

    conta de energia

--- src/app/contas/contas.component.ts
+++ src/app/contas/contas.component.ts
@@ -36,13 +36,14 @@ export class ContasComponent {
     }
     
     calcCom(){
-      this.numComodos = this.numComodos * 15;
+      this.numComodos = this.numComodos * 20; //Cada cômodo tem seu próprio gasto dependendo da quantidade de eletrodoméstico em cada cômodo, o valor utilizado foi uma média entre eles
     }
     calcTV(){
-      this.numTV = this.numTV * 20;
+      this.numTV = this.numTV * 20; //A TV dependendo do modelo tambem varia o valor do KW, o valor utilizado foi de uma TV de 32 polegadas, 6 horas ligadas
     }
     calcPC(){
-      this.numPC = this.numPC * 50;
+      this.numPC = this.numPC * 48; //Aqui a mesma coisa, depende muito de cada computador, a conta utilizada foi 200 Watts (consumo médio) * 8 horas por dia = 1.600 Wh por dia
+                                    //1.600 Wh por dia * 30 dias = 48.000 Wh ou 48 kWh por mês
     }
     calcChuveiro(){
       this.numChuveiro = this.numPessoas * 16.50 //em média, 10 minutos de banho daria 0,55 KWs * 30 (média de 1 mês)
@@ -56,8 +57,9 @@ export class ContasComponent {
       const numPessoas = this.numPessoas;
       const numChuveiro = this.numChuveiro;
       const Tarifa = this.numTarifa;
-      const temMaqLavar = this.MaqLavar === 'true' ? 10 : 0;
-      const temSecadora = this.Secadora === 'true' ? 15 : 0;
+      const temMaqLavar = this.MaqLavar === 'true' ? 10 : 0; //A conta original seria 400 Watts (consumo médio) * 15 ciclos de lavagem = 6.000 Wh ou 6 kWh por mês.
+                                                             //Mas por conta que pode ocorrer mais lavagens do que 1 dia sim e outra não, fora panos, se caso tiver animal em casa, foi aumentado para 10
+      const temSecadora = this.Secadora === 'true' ? 60 : 0; //4.000 Watts (consumo médio) * 15 ciclos de secagem = 60.000 Wh ou 60 kWh por mês
 
       // Chame as funções calcCom(), calcPC() e calcTV() para calcular seus respectivos valores
       this.calcCom();
